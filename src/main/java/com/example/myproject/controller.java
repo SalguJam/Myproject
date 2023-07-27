33617949d3d6d2c2bff2cb7eb6180ef036ca74e3
SalguJam <afterblow23@gmail.com>
2023-07-27 16:16:59 +0900

    first commit

---
+++ src/main/java/com/example/myproject/controller.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class controller {
+    @GetMapping
+    public String hello() {
+        return "Hello World!!";
+    }
+}
